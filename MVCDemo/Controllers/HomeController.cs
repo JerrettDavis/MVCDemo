d6fc16c6854b9d1760a2f0c2e939ccc4f5b7cb5f
JD <jerrett.davis@outlook.com>
2019-09-10 17:33:32 -0500

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using System.Diagnostics;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new AuthenticationModel();
+            return View(model);
+        }
+
+        public IActionResult Index(AuthenticationModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            return RedirectToAction(nameof(Success));
+        }
+
+        public IActionResult Success()
+        {
+            return Json(true);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
